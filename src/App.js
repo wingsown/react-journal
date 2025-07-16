3d237731f2d011d1be9ed36e545a7e8e347073dd
Wilson Buena <wilsonjeffrey.buena@synpulse.com>
2025-07-16 21:27:11 +0800

    add preloaders

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Home from "./components/Home"
 import Journal from "./components/Journal"
 import Photos from "./components/Photos"
 
+import { preloader } from "./utils/preloader"
+
+preloader()
+
 function App() {
   return (
     <Router>
