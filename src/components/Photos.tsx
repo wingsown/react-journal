124c1387087cefa40b28bca1b8bc94a08e35e67e
Wilson Buena <wilsonjeffrey.buena@synpulse.com>
2025-07-26 13:49:44 +0800

    adjust styles

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -64,7 +64,7 @@ const Photos = () => {
 
   useEffect(() => {
     if (loading) {
-      const timeout = setTimeout(() => setLoading(false), 7000)
+      const timeout = setTimeout(() => setLoading(false), 4000)
       return () => clearTimeout(timeout)
     }
   }, [loading])
