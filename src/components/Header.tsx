af33b8d880f0502bdae5c1f20030dd3c656146c7
Wilson Buena <wilsonjeffrey.buena@synpulse.com>
2025-07-27 11:29:18 +0800

    cleanup on headers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,12 +14,6 @@ const Header: React.FC = () => {
     <header className="header" id="header">
       <nav className="nav container">
         <a href="/" className="nav__logo">
-          {/* <img
-            src="assets/img/Icon_4.png"
-            alt=""
-            className="nav__logo-img logo-img"
-            id="logo-img"
-          /> */}
           Journal
         </a>
 
